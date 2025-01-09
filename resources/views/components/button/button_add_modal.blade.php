5bcbe11b3f8135437fc903977ee6aa81924b23a6
agungricky07.github.io <agungricky07@gmail.com>
2025-01-09 11:10:46 +0700

    09/01/2025 Tampilan program belajar & sekolah selesai

--- resources/views/components/button/button_add_modal.blade.php
+++ resources/views/components/button/button_add_modal.blade.php
@@ -1,2 +1,2 @@
 <button type="button" class="add btn btn-primary mb-3" data-bs-toggle="modal"
-data-bs-target="{{$id}}"><i class="fas fa-plus"></i>{{$message}}</button>
+data-bs-target="{{$id}}"><i class="fas fa-plus"></i> {{$message}}</button>
---
+++ resources/views/components/form/input_textArea.blade.php
@@ -0,0 +1,4 @@
+<div class="form-group">
+    <label for="{{$name}}">{{$label}}</label>
+    <textarea style="height: 150px;" class="form-control" id="{{$name}}" name="{{$name}}" cols="30" rows="10"></textarea>
+</div>
