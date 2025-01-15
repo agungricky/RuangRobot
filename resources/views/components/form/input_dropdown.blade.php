084d8adb0df51ba5c2533555553125712e9d6fa8
ikadaniati26 <ikadaniati26@gmail.com>
2025-01-15 09:49:31 +0700

    kelas update

--- resources/views/components/form/input_dropdown.blade.php
+++ resources/views/components/form/input_dropdown.blade.php
@@ -1,8 +1,12 @@
 <div class="form-group">
     <label for="{{ $name }}">{{ $label }}</label>
     <select class="form-control" name="{{ $name }}" id="{{ $name }}">
-        @foreach ($option as $key => $value)
-            <option value="{{ $value }}">{{ $key }}</option>
-        @endforeach
+        {{-- @foreach ($option as $key => $value) --}}
+        {{-- <option value="{{ $value }}">{{ $key }}</option> --}}
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+        <option value="option3">Option 3</option>
+        {{-- @endforeach --}}
     </select>
 </div>
+
